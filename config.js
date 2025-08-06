d79f03f2509557645c7b05cd1b9932a56d868033
kavishvihanga <vihangakavish19940928@gmail.com>
2025-08-06 22:17:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐒𝐔𝐋𝐀-𝐌𝐃=NrV0TbwC#Cs5VHloeUXRftMzvyvBtDeDBrejf5PSoiDL1zHAZT84", // ඔයාගේ session id එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "", // ඔයාගේ session id එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://Sarkar123:GAUTAMMISHRA@sarkar.1uiwqkd.mongodb.net/?retryWrites=true&w=majority", // ඔයාගේ mongodb url එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/TxYQ19zs/SulaMd.jpg",
 BOT_NAME: process.env.BOT_NAME || "sula-md",
