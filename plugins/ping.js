e512df4ff4a5e45069bb76273682b6509b468072
kavishvihanga <vihangakavish19940928@gmail.com>
2025-08-06 10:44:19 -0400

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const message = await conn.sendMessage(from, { text: '> *PINGING...*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `> *𝐒ᴜʟᴀ 𝐌ᴅ  𝐒ᴘᴇᴇᴅ : ${ping}ms 🍷*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `> * CYBER X MD 𝐒ᴘᴇᴇᴅ : ${ping}ms 🍷*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
