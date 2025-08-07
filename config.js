411e49b9e9db6d2dfa30e8badd52aca8f48a4a50
kavishvihanga <vihangakavish19940928@gmail.com>
2025-08-07 17:31:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // ඔයාගේ session id එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "NrV0TbwC#Cs5VHloeUXRftMzvyvBtDeDBrejf5PSoiDL1zHAZT84", // ඔයාගේ session id එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://Sarkar123:GAUTAMMISHRA@sarkar.1uiwqkd.mongodb.net/?retryWrites=true&w=majority", // ඔයාගේ mongodb url එක දාන්න
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/TxYQ19zs/SulaMd.jpg",
-BOT_NAME: process.env.BOT_NAME || "sula-md",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/vxy5kqm0/SulaMd.jpg",
+BOT_NAME: process.env.BOT_NAME || "Cyber-X-MD",
 LANG: process.env.BOT_LANG || 'EN' ,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
