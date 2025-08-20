f0f7d988996c5b4b9e19c36eef7b8d342d318ba2
vihanga-123 <vihangakaviah1234@gmail.com>
2025-08-19 22:53:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐒𝐔𝐋𝐀-𝐌𝐃=R6lh2DDa#owVbRXLPDxHy4r8qIgjk2UEDxJijcmfUfOZS7UG2uOA", // ඔයාගේ session id එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "𝐒𝐔𝐋𝐀-𝐌𝐃=gudWwK4R#8yXjz-v2TaSLyzkAfFwAPxz9UKMXmAXfVLLCgL00Rqo", // ඔයාගේ session id එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://Sarkar123:GAUTAMMISHRA@sarkar.1uiwqkd.mongodb.net/?retryWrites=true&w=majority", // ඔයාගේ mongodb url එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/dHwhYTC/SulaMd.jpg",
 BOT_NAME: process.env.BOT_NAME || "𝐒𝐔𝐋𝐀-𝐌𝐃",
